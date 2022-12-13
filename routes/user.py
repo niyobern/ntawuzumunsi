bf2d15a1d5efe8bc713e076706ca1e724aa23872
niyobern <niyobern@outlook.com>
2022-12-13 12:05:39 +0200

    added something

--- routes/user.py
+++ routes/user.py
@@ -45,10 +45,14 @@ def update_user(id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)
 
 
 @router.get('/{id}')
-def get_user(id: int, db: Session = Depends(get_db), ):
+def get_user(id: int, db: Session = Depends(get_db), user: schemas.User = Depends(oauth2.get_current_user)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"User with id: {id} does not exist")
 
+    return user
+
+@router.get('/current')
+def return_user(user: schemas.User = Depends(oauth2.get_current_user)):
     return user
