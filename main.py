ddc605e8f59bdd63e5a52e15cfd6726106951bd9
niyobern <niyobern@outlook.com>
2022-12-20 23:18:46 +0200

    added aws as allowed origin

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["POST", "GET", "PUT", "DELETE"],
-    allow_headers=["*"],
+    allow_headers=["Authorisation", "content-type","*"],
 )
 
 app.include_router(auth.router)
