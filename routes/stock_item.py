9124b22e6ee6e5161e785e53fba2ef7d47747519
niyobern <niyobern@outlook.com>
2022-12-21 18:00:47 +0200

    fixed an issue of dates

--- routes/stock_item.py
+++ routes/stock_item.py
@@ -19,7 +19,7 @@ def get_stock(db: Session = Depends(get_db), current_user: schemas.User = Depend
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="you have no priviledge to view this")
     if current_user.role.value not in ("boss", "deputy_boss", "store_keeper", "manager"):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not allowed")
-    items = db.query(models.StockItem).filter(models.Cash.created_at.between(start, end)).filter(models.StockItem.name.contains(search)).limit(limit).offset(skip).all()
+    items = db.query(models.StockItem).filter(models.StockItem.created_at.between(start, end)).filter(models.StockItem.name.contains(search)).limit(limit).offset(skip).all()
 
     return items
 
