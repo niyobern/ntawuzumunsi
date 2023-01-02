12b4174fc299f98ecb02bbe341e2017e3c0f658a
niyobern <niyobern@outlook.com>
2023-01-01 19:02:42 -0800

    deploy

--- routes/summary.py
+++ routes/summary.py
@@ -28,6 +28,18 @@ def get_summary(db: Session = Depends(get_db), current_user : schemas.User = Dep
         values = [x.price for x in purchases]
         value = sum(values)
         results.append({"title": "Purchases", "value1": f"{purchases_count} Sales valued {value}", "value2": {"from": start, "end": end}})
+    if current_user.role.value in ("manager", "boss", "deputy_boss", "store_keeper"):
+        purchases = db.query(models.MaterialRequest, models.StockItem).filter(models.StockItem.id == models.MaterialRequest.stock_id).filter(models.MaterialRequest.created_at.between(start, end)).all()
+        purchases_count = len(purchases)
+        values = [x.price for x in purchases]
+        value = sum(values)
+        results.append({"title": "Items Requested From Stock", "value1": f"{purchases_count} with {value} value", "value2": {"from": start, "end": end}})
+    if current_user.role.value in ("manager", "boss", "deputy_boss", "store_keeper"):
+        purchases = db.query(models.MaterialRequest, models.StockItem).filter(models.StockItem.id == models.MaterialRequest.stock_id).filter(models.MaterialRequest.created_at.between(start, end)).all()
+        purchases_count = len(purchases)
+        values = [x.price for x in purchases]
+        value = sum(values)
+        results.append({"title": "Items Gone out of Stock", "value1": f"{purchases_count} with {value} value", "value2": {"from": start, "end": end}})
     return results
 
 
