1824de4a26c97837f259029b528dd90abd3a8821
niyobern <niyobern@outlook.com>
2023-01-02 10:29:38 +0200

    changed the pydantic model for token

--- routes/auth.py
+++ routes/auth.py
@@ -9,7 +9,7 @@ from utils import  utils, oauth2, schemas
 router = APIRouter(tags=['Authentication'])
 
 
-@router.post('/login', response_model=schemas.Token)
+@router.post('/login')
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
 
     user = db.query(models.User).filter(
