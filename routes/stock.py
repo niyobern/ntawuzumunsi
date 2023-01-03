7206a789d0ce135899f579860f5e31e856acaa54
niyobern <niyobern@outlook.com>
2023-01-03 22:20:04 +0200

    testing

--- routes/stock.py
+++ routes/stock.py
@@ -20,9 +20,10 @@ def get_stock(db: Session = Depends(get_db), current_user: schemas.User = Depend
     if current_user.role.value not in ("boss", "deputy_boss", "store_keeper", "manager"):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not allowed")
     items = db.query(models.StockItem, models.Requisition).filter(models.Requisition.stock_id == models.StockItem.id).filter(models.StockItem.created_at.between(start, end)).filter(models.StockItem.name.contains(search)).limit(limit).offset(skip).all()
-    for item in items:
-        del item.created_at
-        del item.creator
+    # items_info = []
+    # for item in items:
+    #     info = {"Stock_id": item.id, "Price": item.price, "Quantity": item.quantity, "Unit": item.unit, "description": item.description}
+    #     items_info.append(info)
     return items
 
 @router.get('/{id}')
