c97c6bac2d1c1ba4f7a1cc3981e8a54c9dcf4e32
niyobern <niyobern@outlook.com>
2023-01-05 00:35:50 +0200

    redeploy

--- routes/auth.py
+++ routes/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException, Response
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from utils import schemas
+from sqlalchemy import or_
 
 from database import database, models
 from utils import  utils, oauth2, schemas
@@ -12,16 +13,15 @@ router = APIRouter(tags=['Authentication'])
 @router.post('/login')
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
 
-    user = db.query(models.User).filter(
-        models.User.email == user_credentials.username or models.User.phone == user_credentials.username).first()
+    user = db.query(models.User).filter(or_(models.User.email == user_credentials.username, models.User.phone == user_credentials.username)).first()
 
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Username")
 
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Password")
 
     # create a token
     # return token
