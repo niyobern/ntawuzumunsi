ecf9b44cfd57247b02fbd20f58eebc93e1b44101
niyobern <niyobern@outlook.com>
2023-01-04 05:07:08 +0200

    removing the typo in skip variable

--- routes/sale.py
+++ routes/sale.py
@@ -4,14 +4,13 @@ from database import models
 from utils import schemas, utils, oauth2
 from database.database import get_db
 from typing import Optional
-import datetime
 from typing import List
 
 router = APIRouter(prefix="/sales", tags=['Sales'])
 
 @router.get('/')
 def get_sales(db: Session = Depends(get_db), current_user : schemas.User =Depends(oauth2.get_current_user),
-  limit: int = 10, skip: int = 10, search: Optional[str] = "", start: str = "2022-12-18", end: str = "2023-12-18"):
+  limit: int = 10, skip: int = 0, search: Optional[str] = "", start: str = "2022-12-18", end: str = "2023-12-18"):
     if current_user.role.value not in ("manager", "boss", "deputy_boss", "retailer"):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Allowed to view this")
     sales = db.query(models.Sale).filter(models.Sale.created_at.between(start, end)).filter(models.Sale.tag.contains(search)).limit(limit).offset(skip).all()
