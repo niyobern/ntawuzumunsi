a84ce93a7656aa80a3eccef19ab760903c19e36d
niyobern <niyobern@outlook.com>
2023-01-07 21:37:36 +0200

    done

--- routes/reports/purchases.py
+++ routes/reports/purchases.py
@@ -61,7 +61,7 @@ async def make_document(title, data, email):
     return "done"
 
 @router.post("/")
-async def send_file(
+def send_file(
     background_tasks: BackgroundTasks,
     email:EmailStr,db: Session = Depends(get_db), current_user: schemas.User = Depends(
     oauth2.get_current_user), limit: int = 100, skip: int = 0, start: str = "2022-12-18", end: str = "2023-12-30"
@@ -77,7 +77,8 @@ async def send_file(
         created_time = str(item.created_at)
         info = {"id": item.id, "stock_id": stock_item.name, "quantity": item.quantity, "created_at": created_time[:10], "tag": item.tag}
         items_info.append(info)
-    background_tasks.add_task(make_document,"Purchases",items_info,email)
+    title = "Purchases"
+    background_tasks.add_task(make_document,title,items_info,email)
 
     return JSONResponse(status_code=200, content={"message": "Your report is being processed"})
 
